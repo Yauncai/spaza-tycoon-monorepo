c26bb6f4166dc75b987eb8e8a4e8c62495a45068
Yauncai <yankho-chantel-maria.saliji@iu-study.org>
2026-01-23 18:47:59 +0200

    updated config file

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,6 +1,26 @@
+// import type { HardhatUserConfig } from "hardhat/config";
+// import "@nomicfoundation/hardhat-toolbox";
+// import "@openzeppelin/hardhat-upgrades"; 
+// import * as dotenv from "dotenv";
+
+// dotenv.config();
+
+// const config: HardhatUserConfig = {
+//   solidity: "0.8.20",
+//   defaultNetwork: "localhost",
+//   networks: {
+//     "base-sepolia": {
+//       url: "https://sepolia.base.org",
+//       accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+//     },
+//   },
+// };
+
+// export default config;
+
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import "@openzeppelin/hardhat-upgrades"; 
+import "@openzeppelin/hardhat-upgrades";
 import * as dotenv from "dotenv";
 
 dotenv.config();
@@ -8,13 +28,33 @@ dotenv.config();
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
   defaultNetwork: "localhost",
+
   networks: {
     "base-sepolia": {
       url: "https://sepolia.base.org",
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      accounts:
+        process.env.PRIVATE_KEY !== undefined
+          ? [process.env.PRIVATE_KEY]
+          : [],
     },
   },
+
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY || "",
+    customChains: [
+      {
+        network: "baseSepolia",
+        chainId: 84532,
+        urls: {
+          apiURL: "https://api-sepolia.basescan.org/api",
+          browserURL: "https://sepolia.basescan.org",
+        },
+      },
+    ],
+  },
+
 };
 
 export default config;
+
 
